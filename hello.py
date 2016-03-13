03c5b034e7fbdf5a24ec82de947e3a105523dc6b
rbcolson9 <rbcolson9@gmail.com>
2016-03-13 11:18:40 -0700

    Added route

--- hello.py
+++ hello.py
@@ -13,16 +13,26 @@ def hello():
 
 @app.route("/liam")
 def things():
-	return """
-	<html>
-	<b>Liam's Page is big and stuff</b>
-	</html>
-	"""
+    return """
+    <html>
+    <b>Liam's Page is big and stuff u know</b>
+    </html>
+    """
+@app.route("/titan")
+def aroute():
+    return """
+    <html>
+    <b>My name is Titan.</b>
+    </html>
+    """
+
 @app.route('/by100/<int:num>')
 def show_post(num):
     # this adds things
     return 'You Multiplied %d x 100 = %d' % (num, num*100)
 
 
+
 if __name__ == "__main__":
     app.run(debug=True)
+
